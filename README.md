e74b381161af8b24bb8189e429a4a19c653b3f09
Vincenzo Stornanti <48057622+Vonvikken@users.noreply.github.com>
2020-04-01 17:54:13 +0200

    Added clean-requirements hook.

--- README.md
+++ README.md
@@ -5,4 +5,12 @@
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-Various pre-commit Git hooks.
+Pre-commit Git hooks I use in my internal projects.
+
+Works with Python 3.6 or newer. Can be used in conjunction with [pre-commit](https://github.com/pre-commit/pre-commit).
+
+## Content
+So far, there's only one hook.
+
+- `clean-requirements`: Remove unused or wrong entries from requirements.txt. Used mainly to solve
+   [this bug](https://bugs.launchpad.net/ubuntu/+source/python-pip/+bug/1635463).
---
+++ pre_commit_hooks/clean_requirements.py
@@ -0,0 +1,37 @@
+import argparse
+from typing import Sequence, Optional, List
+
+EXCLUDED: List[str] = ["pkg-resources==0.0.0"]
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="Filenames to check")
+    parser.add_argument(
+        "--excluded-entries",
+        dest="excl",
+        nargs="*",
+        help="Additional entries to exclude",
+    )
+    args = parser.parse_args(argv)
+    excluded_entries = EXCLUDED + args.excl
+
+    ret = 0
+
+    for filename in args.filenames:
+        with open(filename, "r+", encoding="UTF-8") as f:
+            lines = f.readlines()
+            f.seek(0)
+            f.truncate()
+            new_lines = [line for line in lines if line.strip() not in excluded_entries]
+            removed = len(lines) - len(new_lines)
+            if removed > 0:
+                print(f"Removed {removed} entries from {filename}")
+                ret = 1
+            f.writelines(new_lines)
+
+    return ret
+
+
+if __name__ == "__main__":
+    exit(main())
